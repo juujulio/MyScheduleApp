3b2e6a4ed8bc6de800c1e561d579ddf5d2d57a62
Julio Eduardo <97368804+juujulio@users.noreply.github.com>
2022-03-15 12:45:15 -0300

    Create the "Contact" class.
    Created the "Contact" class with the attributes "email", "name" and "phone_number" and their getters and setters.

---
+++ MyScheduleApp/Contact.py
@@ -0,0 +1,60 @@
+class Contact:
+    '''
+        A class that instance a contact.
+
+    '''
+
+    def __init__(self, name, phone_number):
+        '''
+        The constructor ensures that the minimum information is presented.
+
+
+        :param __name: String
+        :param __phone_number: String (00000-0000) - expected a number with "-" in the middle of the information
+        :param __email: String - expected: example@email.com
+        '''
+
+        self.__name = name
+        self.__email = ''
+        self.__phone_number = phone_number
+
+        print("The class 'Contact' was successfully instantiated.")
+        pass
+
+
+    def setName(self, name):
+        '''A Set method to change the attribute 'name'. '''
+        self.__name = name
+        pass
+
+    def setEmail(self, email):
+        '''A Set method to change the attribute 'email'. '''
+        self.__email = email
+        pass
+
+    def setPhoneNumber(self, phone_number):
+        '''A Set method to change the attribute 'phone_number'. '''
+        self.__phone_number = phone_number
+        pass
+
+    def getName(self):
+        '''A get method to display the "name" attribute.'''
+        print("Contact Name:", self.__name)
+        pass
+
+    def getEmail(self):
+        '''A get method to display the "email" attribute.'''
+        print("Email for contact:", self.__email)
+        pass
+
+    def getPhoneNumber(self):
+        '''A get method to display the "phone_number" attribute.'''
+        print("Phone number:", self.__phone_number)
+        pass
+
+    def showContact(self):
+        '''A get method to display all the informations of the contact.'''
+        print("Name:", self.__name)
+        print("Email for contact:", self.__email)
+        print("Phone number:", self.__phone_number)
+        print("===================================================\n")
---
+++ tests/TestingApp.py
@@ -0,0 +1,10 @@
+from MyScheduleApp.Contact import Contact
+
+Rodrigo = Contact("Rodrigo", "99426-7777")
+Rodrigo.showContact()
+
+Zara = Contact("Zara", "3883-3555")
+Zara.showContact()
+
+Mario = Contact("Mario", "3422-2333")
+
